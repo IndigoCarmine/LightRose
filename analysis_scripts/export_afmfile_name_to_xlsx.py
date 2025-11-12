19e0230e655d9312d7c07d48b19c3f266fd39d5e
IndigoCarmine <tamanisikaminai@gmail.com>
2025-11-12 23:53:23 +0900

    add gitignnore

--- analysis_scripts/export_afmfile_name_to_xlsx.py
+++ analysis_scripts/export_afmfile_name_to_xlsx.py
@@ -1,22 +1,36 @@
 import os
 
 
+def is_afm_file(file_path: str) -> bool:
+    if file_path.endswith("spm"):
+        return True
+    ext = file_path.split(".")[-1]
+    if len(ext) == 3 and ext.isdigit():
+        return True
+    return False
+
+
 def main():
-    afm_directory = "\\\\YagaiG-NAS1\\AFM"
+    afm_directory = r"D:\Dropbox\測定データ\AFM"
     output_excel_file = "export.xlsx"
 
     afm_file_names = []
     for root, _, files in os.walk(afm_directory):
         for file_name in files:
-            if not file_name.endswith("bin") or not file_name.startswith("@"):
+            if file_name.startswith("@"):
+                # Skip temporary or hidden files
+                continue
+            if is_afm_file(file_name):
                 rel_path = os.path.relpath(os.path.join(root, file_name), afm_directory)
-                if os.path.isfile(rel_path):
+                full_path = os.path.join(afm_directory, rel_path)
+                if os.path.isfile(full_path):
                     afm_file_names.append(rel_path)
 
+    print("Total AFM files found:", len(afm_file_names))
     import pandas as pd
 
     df = pd.DataFrame(afm_file_names, columns=["AFM File Names"])
-    df.to_excel(output_excel_file, index=False)
+    df.to_excel(output_excel_file)
 
 
 if __name__ == "__main__":
